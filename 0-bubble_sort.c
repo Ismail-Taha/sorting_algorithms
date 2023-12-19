5b0d17c30b16dbca1a279121acc74b9221154bd7
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-12-18 16:51:57 -0800

    first sorting

---
+++ 0-bubble_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * switch_int - Switch two integers in an array.
+ * @a: The first integer to switch.
+ * @b: The second integer to switch.
+ */
+void switch_int(int *a, int *b)
+{
+	int cp;
+
+	cp = *a;
+	*a = *b;
+	*b = cp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t pass, indx;
+
+	if (array == NULL || !size)
+		return;
+
+	pass = 0;
+	while (pass < size)
+	{
+		for (indx = 0; indx < size - 1; indx++)
+		{
+			if (array[indx] > array[indx + 1])
+			{
+				switch_int(array + indx, array + indx + 1);
+				print_array(array, size);
+			}
+		}
+		pass++;
+	}
+}
